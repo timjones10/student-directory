99af674ea3950e155e43f1c0bf274d4cfd1549d6
Tim Jones <timjones10@hotmail.com>
2017-09-11 14:32:27 +0100

    Comments added

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of students
 puts "The students of Villains Academy"
 puts "-----------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,8 @@ puts "Freddy Kreuger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# followed by a statement of the total number
 print "Overall, we have "
+# print has been used here so that the line break is not added
 print 11
 puts " great students"
